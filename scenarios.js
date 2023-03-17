41395d954e0ceadaf66c586081f9a63e8a79408f
Boris Dalstein <dalboris@gmail.com>
2023-03-17 18:58:51 +0100

    Add controls for all edge angles; Add default animation for all scenarios; Various fixes

--- scenarios.js
+++ scenarios.js
@@ -3,7 +3,7 @@ var scenarios = [
         prettyName: "2 edges of different widths",
         edges: [
             { 'radius': 30, 'angle': 0, 'color': 'blue',
-              'angleAnim': [[10, 360], [0, 0], [6, 360], [0, 0], [3, 360], [0, 0], [1, 360]],
+              'angleAnim': [[10, 360], [0, 0]],
               'relRadiusAnim': [[10, 2.5], [0, 1], [10, 2.5]] },
             { 'radius': 200, 'angle': 180, 'color': 'red' },
         ]
@@ -11,24 +11,27 @@ var scenarios = [
     {
         prettyName: "2 edges of same widths",
         edges: [
-            { 'radius': 50, 'angle': 0, 'color': 'blue' },
+            { 'radius': 50, 'angle': 0, 'color': 'blue',
+              'angleAnim': [[10, 360], [0, 0]] },
             { 'radius': 50, 'angle': 160, 'color': 'red' },
         ]
     },
     {
         prettyName: "3 edges of different widths",
         edges: [
-            { 'radius': 40, 'angle': 0, 'color': 'blue' },
-            { 'radius': 100, 'angle': 0, 'color': 'red' },
-            { 'radius': 70, 'angle': 1, 'color': 'green' },
+            { 'radius': 40, 'angle': 0, 'color': 'blue',
+              'angleAnim': [[10, 360], [0, 0]] },
+            { 'radius': 100, 'angle': 80, 'color': 'red' },
+            { 'radius': 70, 'angle': 210, 'color': 'green' },
         ]
     },
     {
         prettyName: "3 edges: t-junction (narrow onto wide)",
         edges: [
-            { 'radius': 40, 'angle': Math.PI / 2, 'color': 'blue' },
+            { 'radius': 40, 'angle': 70, 'color': 'blue',
+              'angleAnim': [[5, 110], [5, 70]] },
             { 'radius': 100.001, 'angle': 0, 'color': 'red' },
-            { 'radius': 100, 'angle': Math.PI, 'color': 'green' },
+            { 'radius': 100, 'angle': 180, 'color': 'green' },
         ]
     },
 ];
